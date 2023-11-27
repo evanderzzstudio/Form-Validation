9f17f780e189f68ccf1b0ecf2f720da0990ffda7
evanderzzstudio <evanderstudio@gmail.com>
2023-11-27 10:41:03 +0700

    Update form validation logic and error handling.

--- script.js
+++ script.js
@@ -4,20 +4,38 @@ const form = document.querySelector("#form")
 const usernameInput = document.querySelector("#username")
 const passwordInput = document.querySelector("#password")
 const passwordConfirmInput = document.querySelector("#password-confirmation")
-const termsInput = document.querySelector("#username")
+const termsInput = document.querySelector("#terms")
 
 // TODO: Create an event listener for when the form is submitted and do the following inside of it.
 form.addEventListener("submit", e => {
+  //    TODO: Create an array to store all error messages and clear any old error messages
+  const errorMessages = []
+  e.preventDefault()
+  clearErrors()
+  //    TODO: Define the following validation checks with appropriate error messages
+  //      1. Ensure the username is at least 6 characters long
+  if (usernameInput.value.length < 6) {
+    errorMessages.push("Username must be at least 6 characters")
+  }
+  //      2. Ensure the password is at least 10 characters long
+  if (passwordInput.value.length < 10) {
+    errorMessages.push("Password must be at least 10 characters")
+  }
+  //      3. Ensure the password and confirmation password match
+  if (passwordConfirmInput.value !== passwordInput.value) {
+    errorMessages.push("Password must match")
+  }
+  //      4. Ensure the terms checkbox is checked
+  if ()
+    //    TODO: If there are any errors then prevent the form from submitting and show the error messages
 
+    if (errorMessages.length > 0) {
+      e.preventDefault()
+      showErrors(errorMessages)
+    }
+
+  console.log(errorMessages)
 })
-//    TODO: Create an array to store all error messages and clear any old error messages
-//    TODO: Define the following validation checks with appropriate error messages
-//      1. Ensure the username is at least 6 characters long
-//      2. Ensure the password is at least 10 characters long
-//      3. Ensure the password and confirmation password match
-//      4. Ensure the terms checkbox is checked
-//    TODO: If there are any errors then prevent the form from submitting and show the error messages
-
 // TODO: Define this function
 function clearErrors() {
   // Loop through all the children of the error-list element and remove them
