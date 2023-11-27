f23f001cae8af0b84b056e830be5c44d85b4f69f
evanderzzstudio <evanderstudio@gmail.com>
2023-11-27 11:17:30 +0700

    - Update error handling logic and display.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const usernameInput = document.querySelector("#username")
 const passwordInput = document.querySelector("#password")
 const passwordConfirmInput = document.querySelector("#password-confirmation")
 const termsInput = document.querySelector("#terms")
+const errorsContainer = document.querySelector(".errors")
+const errorsList = document.querySelector(".errors-list")
 
 
 // TODO: Create an event listener for when the form is submitted and do the following inside of it.
@@ -46,6 +48,7 @@ function clearErrors() {
   // I recommend using a while loop to accomplish this task
   // This is the trickiest part of this exercise so if you get stuck and are unable to progress you can also set the innerHTML property of the error-list to an empty string and that will also clear the children. I recommend trying to accomplish this with a while loop, though, for practice.
   // Also, make sure you remove the show class to the errors container
+  errorsList.innerHTML = ""
 }
 
 // TODO: Define this function
@@ -53,4 +56,10 @@ function showErrors(errorMessages) {
   // Add each error to the error-list element
   // Make sure to use an li as the element for each error
   // Also, make sure you add the show class to the errors container
+  errorMessages.forEach(errorMessage => {
+    const li = document.createElement("li")
+    li.innerText = errorMessage
+    errorsList.appendChild(li)
+  })
+  errorsContainer.classList.add("show")
 }
